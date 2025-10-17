6cf9975d6626fbfd57830dbe1ecbf0509be83e2c
Wcowin <1135801806@qq.com>
2025-10-17 11:40:28 +0800

    Update to v1.2.9 https://gitee.com/Wcowin/OneClip/releases/download/#{version}/OneClip-#{version}.dmg (Universal Binary)

--- Casks/oneclip.rb
+++ Casks/oneclip.rb
@@ -1,19 +1,19 @@
 cask "oneclip" do
-  version "1.2.7"
-  sha256 "7689c0596f0b44530a59d384cdeb133e38f6fc7767fdda416310325a12a4530c"
+  version "1.2.9"
+  sha256 :no_check  # Gitee 不支持自动校验，使用 :no_check
 
-  url "https://gitee.com/Wcowin/OneClip/releases/download/#{version}/OneClip-#{version}-apple-silicon.dmg"
+  url "https://gitee.com/Wcowin/OneClip/releases/download/#{version}/OneClip-#{version}.dmg"
   name "OneClip"
-  desc "Professional clipboard manager"
+  desc "Professional clipboard manager for macOS"
   homepage "https://oneclip.cloud/"
 
   livecheck do
     url "https://gitee.com/Wcowin/OneClip/releases"
-    regex(/OneClip[._-]v?(\d+(?:\.\d+)+)[._-]apple[._-]silicon\.dmg/i)
+    regex(/OneClip[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
   depends_on macos: ">= :monterey"
-  depends_on arch: :arm64
+  # 通用版本，支持所有架构
 
   app "OneClip.app"
 
