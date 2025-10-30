990030bddadf139b2de2b56760ed5d25ee411415
Wcowin <1135801806@qq.com>
2025-10-31 00:04:31 +0800

    Update oneclip.rb

--- Casks/oneclip.rb
+++ Casks/oneclip.rb
@@ -1,5 +1,5 @@
 cask "oneclip" do
-  version "1.2.7"
+  version "1.3.1"
   sha256 :no_check  # Gitee 不支持自动校验，使用 :no_check
 
   url "https://gitee.com/Wcowin/OneClip/releases/download/#{version}/OneClip-#{version}-apple-silicon.dmg"
