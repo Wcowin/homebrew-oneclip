fdea239e98c8f15d58b8f51e5e9134d3d854f874
Wcowin <1135801806@qq.com>
2025-12-08 11:18:44 +0800

    fix: 将下载地址从 GitHub 改为 Gitee，解决国内用户无法下载问题

--- Casks/oneclip.rb
+++ Casks/oneclip.rb
@@ -2,13 +2,13 @@ cask "oneclip" do
   version "1.3.9"
   sha256 "cf510a054bfac05f85bb3cf5bf7607065a456e4a01b3e362bec398d296e94eb6"
 
-  url "https://github.com/Wcowin/OneClip/releases/download/#{version}/OneClip-#{version}.dmg"
+  url "https://gitee.com/Wcowin/OneClip/releases/download/#{version}/OneClip-#{version}.dmg"
   name "OneClip"
   desc "Professional clipboard manager for macOS"
   homepage "https://oneclip.cloud/"
 
   livecheck do
-    url "https://github.com/Wcowin/OneClip/releases"
+    url "https://gitee.com/Wcowin/OneClip/releases"
     regex(/OneClip[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
