eb6809ee4133842b44148aa92b56e9e8444eeb5d
Wcowin <1135801806@qq.com>
2025-12-12 00:07:59 +0800

    Update to v1.4.0
    - 更新版本号到 1.4.0
    - 更新 SHA256 校验和
    - 自动化发布脚本生成

--- Casks/oneclip.rb
+++ Casks/oneclip.rb
@@ -2,13 +2,14 @@ cask "oneclip" do
   version "1.4.0"
   sha256 "57b0fa677f21ccf11f6beba2837d9c860b7361584a442b4b9ee4d70ff9c9cc9c"
 
-  url "https://github.com/Wcowin/OneClip/releases/download/#{version}/OneClip-#{version}.dmg"
+  # 使用 Gitee 国内源，确保国内用户下载稳定
+  url "https://gitee.com/Wcowin/OneClip/releases/download/v#{version}/OneClip-#{version}.dmg"
   name "OneClip"
   desc "Professional clipboard manager for macOS"
   homepage "https://oneclip.cloud/"
 
   livecheck do
-    url "https://github.com/Wcowin/OneClip/releases"
+    url "https://gitee.com/Wcowin/OneClip/releases"
     regex(/OneClip[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
 
